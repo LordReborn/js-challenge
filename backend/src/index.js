10fa9ea5569ea44a0826a6210dfdc61cda8e8213
LordReborn <LordReborn@users.noreply.github.com>
2021-02-22 15:00:30 -0300

    remove mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,6 @@
-const mongoose = require('mongoose');
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-
-const start = async () => {
-try {
-    await mongoose.connect('mongodb+srv://root:root@cluster0.p7d0u.mongodb.net/balance?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-    });
-    console.log('Connected to mongoDB');
-} catch (error) {
-    console.error(error);
-}
 
 // settings
 app.set('port', process.env.PORT || 4000);
@@ -33,7 +19,3 @@ app.use(express.json());
 app.use(require('./routes'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/registry', require('./routes/registry'));
-
-}
-
-start();
